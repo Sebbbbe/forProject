3aff1f14f3cd3ccd731eeaa5e1a14e235c83a0a8
Sebbbbe <sebastian.tiger901@gmail.com>
2021-04-08 12:10:47 +0200

    todays commit

---
+++ Bug_Tracker/Controllers/IssueController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bug_Tracker.Controllers
+{
+    public class IssueController
+    {
+
+        [HttpGet]
+        public List<Models.Issue> Get()
+        {
+            var context = new BugTrackerDBContext();
+            
+            var Issue = context.Issues.Include(x => x.User).Include(x => x.Project).ToList();
+            
+            return Issue;
+          
+          
+        }
+    }
+}
--- Bug_Tracker/Program.cs
+++ Bug_Tracker/Program.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace Bug_Tracker
 {
+  
     public class Program
     {
         public static void Main(string[] args)
